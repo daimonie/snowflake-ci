a29c368778b1778ddaf26056d56ce1e8a906288a
daimonie <info@joskodeboer.nl>
2026-08-20 12:09:51 +0000

    moved setup around

--- log_test.sql
+++ log_test.sql
@@ -3,12 +3,7 @@
 
 SET name = '<% name %>';
 
-USE ROLE GIT_CICD;
-
-CREATE TABLE IF NOT EXISTS log AS
-SELECT 'new table!' AS message, CURRENT_TIMESTAMP() as time_col;
-
-ALTER TABLE plant_shop.public.log MODIFY COLUMN message VARCHAR(256);
+USE ROLE GIT_CICD; 
 
 INSERT INTO plant_shop.public.log (message, time_col) VALUES ('New entry: ' || $name, CURRENT_TIMESTAMP());
 
