f3414436443a01e78740ab615f0d38221645277e
daimonie <info@joskodeboer.nl>
2026-08-20 09:57:18 +0000

    fixed string concat

--- log_test.sql
+++ log_test.sql
@@ -7,6 +7,6 @@ USE ROLE GIT_CICD;
 CREATE TABLE IF NOT EXISTS log AS
 SELECT 'new table!' AS message, CURRENT_TIMESTAMP() as time_col;
 
-INSERT INTO plant_shop.public.log (message, time_col) VALUES ("New entry: $name", CURRENT_TIMESTAMP());
+INSERT INTO plant_shop.public.log (message, time_col) VALUES ('New entry: ' || $name, CURRENT_TIMESTAMP());
 
 SELECT * FROM plant_shop.public.log;
