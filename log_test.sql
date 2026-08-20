53b751b382c1dfde46c1e6d9f761be2f288102c5
daimonie <info@joskodeboer.nl>
2026-08-20 12:23:52 +0000

    apparently the modify seeped back into log_test

--- log_test.sql
+++ log_test.sql
@@ -8,7 +8,6 @@ USE ROLE GIT_CICD;
 CREATE TABLE IF NOT EXISTS log AS
 SELECT 'new table!' AS message, CURRENT_TIMESTAMP() as time_col;
 
-ALTER TABLE plant_shop.public.log MODIFY COLUMN message VARCHAR(256);
 
 INSERT INTO plant_shop.public.log (message, time_col) VALUES ('New entry: ' || $name, CURRENT_TIMESTAMP());
 
